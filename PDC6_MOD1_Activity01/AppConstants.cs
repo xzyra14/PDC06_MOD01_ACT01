69767d0d301a2402b080ea047192096ef0615ab7
Xzyra Lapid <83936947+xzyra14@users.noreply.github.com>
2022-09-12 19:31:15 +0800

    Add files via upload

---
+++ PDC6_MOD1_Activity01/AppConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDC6_MOD1_Activity01
+{
+    static class AppConstants
+    {
+        public static double NormalFontSize = 14;
+        public static double LargeFontSize = 30;
+    }
+}
---
+++ PDC6_MOD1_Activity01/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace PDC6_MOD1_Activity01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await Navigation.PushAsync(page);
+            });
+            BindingContext = this;
+        }
+
+        public ICommand NavigateCommand { private set; get; }
+    }
+}
